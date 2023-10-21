66bb398b6ec93c98eb1bf43f30fe93c2c0c7af48
Ryan Benoit <ryanbenoit.2001@gmail.com>
2023-10-21 18:39:21 -0300

    basic movement and camera follow added

---
+++ Assets/Scripts/Robot/RobotMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    Vector3 startScale;
+
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        startScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = startScale;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(startScale.x * -1, startScale.y, startScale.z);
+    }
+}
